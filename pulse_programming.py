de7ffff5b7f1fe99f20e9318bdd8e2205ff0837c
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-17 23:00:51 +0200

    classifying one color into all

--- pulse_programming.py
+++ pulse_programming.py
@@ -21,14 +21,18 @@ codebook, distortion = vq.kmeans(whitened_colors, NUMBER_OF_COLORS)
 print("codebook", codebook, "distortion", distortion)
 code, distance = vq.vq(whitened_colors, codebook)
 reshaped_code = code.reshape((len(image), len(image[0])))
+print("Assuming background has biggest area.")
 counts = [0] * NUMBER_OF_COLORS
 for cls in code:
     counts[cls] += 1
 max_class = max(range(NUMBER_OF_COLORS), key=lambda i:counts[i])
 classified_image = np.array(
-    [[[255 * (col == max_class)] * 3 for col in row]
+    [[col == max_class for col in row]
      for row in reshaped_code],
-    image.dtype)
+    np.uint8)
+
+print("Eroding image to one pixel lines.")
+
 
 print("classes", set(classified_image.flatten()))
 print("classified_image", classified_image)
