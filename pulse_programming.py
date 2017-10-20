fe29c31c8f0c17d54811da87fe2f0627874a33d9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-20 14:43:14 +0200

    find circles

--- pulse_programming.py
+++ pulse_programming.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 import cv2
 import scipy.cluster.vq as vq
@@ -6,6 +7,8 @@ NUMBER_OF_COLORS = 20
 NUMBER_OF_COLORS += 1 # add background color
 
 image = cv2.imread("example.png")
+
+image = cv2.GaussianBlur(image, (5, 5), 0)
 
 flat_colors = image.reshape((-1, 3))
 maximum_bright_colors = flat_colors
@@ -31,40 +34,34 @@ classified_image = np.array(
      for row in reshaped_code],
     np.uint8)
 
-print("Eroding image to one pixel lines.")
-# from http://felix.abecassis.me/2011/09/opencv-morphological-skeleton/
-# from https://stackoverflow.com/questions/30045166/thinnig-of-a-binary-image-python
-skel = np.zeros((len(classified_image), len(classified_image[0])), np.uint8)
-temp = np.zeros((len(classified_image), len(classified_image[0])), np.uint8)
-element0 = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-element1 = np.array([[0,0,0], [1,1,1], [0,0,0]], np.uint8)
-element2 = np.array([[0,1,0], [0,1,0], [0,1,0]], np.uint8)
-element3 = np.array([[1,0,0], [0,1,0], [0,0,1]], np.uint8)
-element4 = np.array([[0,0,1], [0,1,0], [1,0,0]], np.uint8)
-element5 = np.array([[1,1,1], [1,1,0], [0,0,0]], np.uint8)
-element6 = np.array([[0,1,0], [1,1,1], [0,1,0]], np.uint8)
-element = np.array([[0,0,0], [0,0,1], [0,1,1]], np.uint8)
-
-img = cv2.erode(classified_image, element, iterations=0)
-done = False
-for i in range(20):
-    eroded = cv2.erode(img, element)
-    dilated = cv2.dilate(eroded, element)
-    temp = cv2.subtract(img,dilated)
-    skel = cv2.bitwise_or(skel,temp)
-    img = skel#eroded
-    zeros = cv2.countNonZero(img)
-    print(zeros)
-    if zeros == 0:
-        done = True
-        break
-
-
 print("classes", set(classified_image.flatten()))
 print("classified_image", classified_image)
 
+img = classified_image
 
+element = np.array([[0,0,0],[0,0,1],[1,1,1]], np.uint8)
+
+# compute tops
+img2 = cv2.dilate(img, element)
+img2 = cv2.subtract(img, img2)
+element2 = np.array([[1,1,1],[1,1,1],[1,1,1]], np.uint8)
+img2 = cv2.dilate(img2, element2, iterations=20)
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=75,param2=25,minRadius=20,maxRadius=200)
+print("circles", circles)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(img2,(i[0],i[1]),i[2],128,2)
+    # draw the center of the circle
+    cv2.circle(img2,(i[0],i[1]),2,128,3)
 
 cv2.namedWindow("W1", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("W1", skel)
+cv2.imshow("W1", img)
+
+cv2.namedWindow("W2", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("W2", img2)
+
 cv2.waitKey(0)
