6532584a533d9534eef504950849592f84d6bcc7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-20 23:14:11 +0200

    using colors hardcoded colors to find the clusters

--- pulse_programming.py
+++ pulse_programming.py
@@ -3,51 +3,56 @@ import numpy as np
 import cv2
 import scipy.cluster.vq as vq
 
-NUMBER_OF_COLORS = 2
+NUMBER_OF_COLORS = 3
 NUMBER_OF_COLORS += 1 # add background color
 
 image = cv2.imread("pathxxx.png")
 DIMENSIONS = (len(image), len(image[0]))
 
 image = cv2.GaussianBlur(image, (5, 5), 0)
-
+codebook_raw = [[50,50,50], [150,150,150], [50,0,0], [0,50,0], [0,0,50]]
 flat_colors = image.reshape((-1, 3))
-maximum_bright_colors = flat_colors
-#def maximize_color(color):
-#    m = color.sum() / 3
-#    if m == 0:
-#        return (255, 255, 255)
-#    return [int(v * 255 / 3 // m) for v in color]
-#maximum_bright_colors = np.array(list(map(maximize_color, flat_colors)), flat_colors.dtype)
-print("flat_colors", set(map(tuple, flat_colors)))
-whitened_colors = vq.whiten(maximum_bright_colors)
-codebook, distortion = vq.kmeans(whitened_colors, NUMBER_OF_COLORS)
-print("codebook", codebook, "distortion", distortion)
-code, distance = vq.vq(whitened_colors, codebook)
+whitened = vq.whiten(np.concatenate((flat_colors, np.array(codebook_raw, np.uint8))))
+codebook = whitened[len(flat_colors):]
+print("codebook", codebook)
+all_features = whitened[:len(flat_colors)]
+code, distance = vq.vq(all_features, codebook)
+
 reshaped_code = code.reshape(DIMENSIONS)
 print("Assuming background has biggest area.")
-counts = [0] * NUMBER_OF_COLORS
-for cls in code:
-    counts[cls] += 1
-color_usage_indices = list(range(NUMBER_OF_COLORS))
-color_usage_indices.sort(key=lambda i:counts[i])
-background_class = color_usage_indices[-1]
-road_class = color_usage_indices[-2]
-pulse_source_class = color_usage_indices[-3]
+background_class = 1
+road_source_class = 3 # 0
+bridge_source_class =  3
+pulse_source_class = 2
+pump_source_class = 4
 road_image = np.array(
-    [[255 * (col == road_class) for col in row]
+    [[255 * (col == road_source_class) for col in row]
      for row in reshaped_code],
     np.uint8)
 pulse_source_image = np.array(
     [[255 * (col == pulse_source_class) for col in row]
      for row in reshaped_code],
     np.uint8)
+pump_source_image = np.array(
+    [[255 * (col == pump_source_class) for col in row]
+     for row in reshaped_code],
+    np.uint8)
+bridge_source_image = np.array(
+    [[255 * (col == bridge_source_class) for col in row]
+     for row in reshaped_code],
+    np.uint8)
 
 cv2.namedWindow("road_image", cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow("pulse_source_image", cv2.WINDOW_AUTOSIZE)
-
 cv2.imshow("road_image", road_image)
+cv2.namedWindow("pulse_source_image", cv2.WINDOW_AUTOSIZE)
 cv2.imshow("pulse_source_image", pulse_source_image)
+cv2.namedWindow("pump_source_image", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("pump_source_image", pump_source_image)
+cv2.namedWindow("bridge_source_image", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("bridge_source_image", bridge_source_image)
+
+cv2.waitKey(0)
+exit(0)
 
 # pulse generation
 pulse0 = zeros = np.zeros(DIMENSIONS, np.uint8)
@@ -59,7 +64,7 @@ border_element = np.array([[1,1,1],[1,1,1],[1,1,1]], np.uint8)
 def pulse(state):
     pulse1, pulse0 = state
     pulse2 = cv2.dilate(pulse1, pulse_element)
-
+    pulse2 = cv2.subtract(pulse2, pulse0)
     pulse2 = cv2.bitwise_and(pulse2, expansion_room)
     return pulse2, pulse1
 
