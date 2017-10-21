335dc0e1964d305ba3df1634716292f2d67b5b19
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-21 18:32:02 +0200

    pulse going through gate

--- pulse_programming.py
+++ pulse_programming.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import scipy.cluster.vq as vq
 
-codebook_raw = [[0,0,0], [255, 255, 255], [255,0,0], [0,255,0], [0,0,255]]
+codebook_raw = [[0,0,0], [255, 255, 255], [128,128,128]]
 NUMBER_OF_COLORS = len(codebook_raw)
 
 image = cv2.imread("pathxxx.png")
@@ -25,56 +25,95 @@ def rgb_likelyhood(color):
     """Return deviation from red, green, blue"""
     return (color[1] - color[2])**2, (color[0] - color[2])**2, (color[0] - color[1])**2
 
-def classify(color):
+def how_colored_is_this(color):
     r, g, b = rgb_likelyhood(color)
-    return g + b - r, b + r - g, r + g - b
+    return g + b + r
 
-classified_features = list(map(classify, codebook))
-print("classified_features", np.array(classified_features))
-
-indices = range(len(codebook))
-red = max(indices, key=lambda i: classify(codebook[i])[0])
-green = max(indices, key=lambda i: classify(codebook[i])[1])
-blue = max(indices, key=lambda i: classify(codebook[i])[2])
-
-
-
-background_class = codebook[0]
-road_source_class = codebook[0]
-bridge_source_class = green
-pulse_source_class = blue
-pump_source_class = red
+color_usage = [0] * NUMBER_OF_COLORS
+for cls in code:
+    color_usage[cls] += 1
+background_class = max(range(NUMBER_OF_COLORS), key=lambda i: color_usage[i])
+print("color_usage", color_usage, "background_class", background_class)
+colorful_class = max(range(NUMBER_OF_COLORS), key=lambda i: how_colored_is_this(codebook[i]))
+print("colorful_class", colorful_class)
+assert colorful_class != background_class, "The background must not be colored."
+gate_source_class = colorful_class
+road_source_class = (set(range(NUMBER_OF_COLORS)) - set((colorful_class, background_class))).pop()
 
 road_image = np.array(
     [[255 * (col == road_source_class) for col in row]
      for row in reshaped_code],
     np.uint8)
-pulse_source_image = np.array(
-    [[255 * (col == pulse_source_class) for col in row]
-     for row in reshaped_code],
-    np.uint8)
-pump_source_image = np.array(
-    [[255 * (col == pump_source_class) for col in row]
-     for row in reshaped_code],
-    np.uint8)
-bridge_source_image = np.array(
-    [[255 * (col == bridge_source_class) for col in row]
+gate_image = np.array(
+    [[255 * (col == gate_source_class) for col in row]
      for row in reshaped_code],
     np.uint8)
 
 cv2.namedWindow("road_image", cv2.WINDOW_AUTOSIZE)
 cv2.imshow("road_image", road_image)
-cv2.namedWindow("pulse_source_image", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("pulse_source_image", pulse_source_image)
-cv2.namedWindow("pump_source_image", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("pump_source_image", pump_source_image)
-cv2.namedWindow("bridge_source_image", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("bridge_source_image", bridge_source_image)
-
-cv2.waitKey(0)
-exit(0)
+cv2.namedWindow("gate_image", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("gate_image", gate_image)
 
 # pulse generation
+zeros = np.zeros(DIMENSIONS, np.uint8)
+
+road_pulse_element = np.array([[0,1,0],[1,1,1],[0,1,0]], np.uint8)
+right_pulse_element = np.array([[0,0,0],[1,0,0],[0,0,0]], np.uint8)
+down_pulse_element = np.array([[0,1,0],[0,0,0],[0,0,0]], np.uint8)
+generator_element1 = np.array([[1,0,0],[0,0,0],[0,0,0]], np.uint8)
+generator_element2 = np.array([[0,1,1],[1,1,1],[1,1,1]], np.uint8)
+expand_element = np.array([[1,1,1],[1,1,1],[1,1,1]], np.uint8)
+
+# generate image with only generator_element1
+pulse_input_image = cv2.subtract(
+    cv2.dilate(gate_image, generator_element1),
+    cv2.dilate(gate_image, generator_element2))
+gate_expanded = cv2.dilate(gate_image, expand_element)
+
+cv2.namedWindow("pulse_input_image", cv2.WINDOW_AUTOSIZE)
+cv2.imshow("pulse_input_image", pulse_input_image)
+
+def pulse(state, element, area, input=()):
+    """Make the next pulse using an element on an area, adding the input
+    as a pulse source."""
+    pulse1, pulse0 = state
+    pulse2 = cv2.dilate(pulse1, element)
+    for input_image in input:
+        pulse2 = cv2.bitwise_or(pulse2, input_image)
+    pulse2 = cv2.subtract(pulse2, pulse0)
+    pulse2 = cv2.bitwise_and(pulse2, area)
+    return pulse2, pulse1
+
+cv2.namedWindow("W3", cv2.WINDOW_AUTOSIZE)
+down_pulse = right_pulse = road_pulse = (zeros, zeros)
+while cv2.waitKey(20) != 27: # press escape
+    road_pulse = pulse(road_pulse, road_pulse_element, road_image, (pulse_input_image, down_pulse[0], right_pulse[0]))
+    right_pulse = pulse(right_pulse, right_pulse_element, gate_expanded, (road_pulse[0],))
+    down_pulse = pulse(down_pulse, down_pulse_element, gate_expanded, (road_pulse[0],))
+    image = road_pulse[0]
+    for a_pulse in (right_pulse, down_pulse):
+        image = cv2.bitwise_or(image, a_pulse[0])
+    cv2.imshow("W3", image)
+exit(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 pulse0 = zeros = np.zeros(DIMENSIONS, np.uint8)
 pulse1 = np.zeros(DIMENSIONS, np.uint8)
 
