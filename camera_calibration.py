19929eb1da75223386a4cb408fcf07661f75ffb5
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-10-30 15:03:12 +0100

    refactoring

--- camera_calibration.py
+++ camera_calibration.py
@@ -2,97 +2,14 @@ import cv2
 from camera_image import capture_image_gray, max_resolution
 import numpy as np
 import random
+import time
 
-INTERACTIVE = True
-
-CALIBRATION = "Calibration"
-DIFF = "Difference"
-
-zeros = max_resolution_zeros()
-mh, mw = zeros.shape[:2]
-
-
-def wait():
-    if cv2.waitKey(0) == 27:
-        exit(0)
-
-
-def interact():
-    if INTERACTIVE:
-        print("Move the window and press a key.")
-        wait()
-
-
-cv2.namedWindow(CALIBRATION, cv2.WINDOW_AUTOSIZE)
-cv2.imshow(CALIBRATION, zeros)
-
-interact()
-
-background = capture_image_gray()
-background = background // 2
-
-
-circle_radius = 30
-points = (
-    (circle_radius, circle_radius),
-    (mw - circle_radius, circle_radius),
-    (mw - circle_radius, mh - circle_radius),
-    (circle_radius, mh - circle_radius),
-)
-
-
-images = []
-
-EROSION_SQUARE = np.array([[1,1,1], [1,1,1], [1,1,1]], np.uint8)
-
-cv2.namedWindow(CALIBRATION, cv2.WINDOW_AUTOSIZE)
-
-for x, y in points:
-    image = max_resolution_zeros()
-    cv2.circle(image, (x, y), circle_radius, 255, -1)
-    cv2.imshow(CALIBRATION, image)
-    interact()
-    capture = capture_image_gray()
-    diff = capture // 2 + 128 - background
-    ret, white = cv2.threshold(diff, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    cv2.imshow("Threshold", white)
-    #images.append(white)
-    # could the that connected components is faster
-    count = 40
-    while True:
-        last_white = white
-        white = cv2.erode(white, EROSION_SQUARE, iterations=count)
-        white_pixel_count = cv2.countNonZero(white)
-        print("white_pixel_count", white_pixel_count)
-        if white_pixel_count == 0:
-            white = last_white
-            if count >= 4:
-                count //= 4
-            if count >= 2:
-                count //= 2
-            else:
-                break # we went too far
-    white_positions = cv2.findNonZero(white)
-    xs, ys = cv2.split(white_positions)
-    x = xs.mean()
-    y = ys.mean()
-    print("x, y", x, y)
-    cv2.circle(capture, (int(x), int(y)), circle_radius, (255, 0, 0), 5)
-    cv2.imshow(DIFF, capture)
-
-
-
-
-
-
-if INTERACTIVE:
-    print("Press any key to exit.")
-wait()
-
+EROSION_ELEMENT = np.array([[0,1,0], [1,1,1], [0,1,0]], np.uint8)
 
 class Calibration:
 
-    def __init__(self, resolution="max", circle_radius=30, window_name="Calibration"):
+    def __init__(self, resolution="max", circle_radius=30, window_name="Calibration",
+                 maximum_background_seconds=10):
         """Create a new Calibration window.
 
         Arguments:
@@ -100,15 +17,40 @@ class Calibration:
                     maximum available resolution.
         - circle_radius - the size of the circle to use for calibration.
         - window_name - the name of the window.
+        - maximum_background_seconds - seconds until a new background image
+                                       should be recorced
         """
         self._window_name = window_name
+        if resolution == "max":
+            resolution = max_resolution()
         self._resolution = resolution
+        self._circle_radius = circle_radius
         self._source_points = []
         self._destination_points = []
         self._H = None # Transformation Matrix
+        self._maximum_background_seconds = maximum_background_seconds
+        self._next_background_capture = 0
+        self._background = None
         cv2.namedWindow(self._window_name, cv2.WINDOW_AUTOSIZE)
-        cv2.imshow(self._window_name, self.get_resolution_zeros())
         cv2.moveWindow(self._window_name, 0, 0)
+        self.fill_black()
+
+    def fill_black(self):
+        """Fill the window with black."""
+        cv2.imshow(self._window_name, self.get_resolution_zeros())
+
+    def record_background(self):
+        """Record a background image.
+
+        The background im age is a 7bit gray recorded image.
+        """
+        self._background = capture_image_gray() // 2
+        self._next_background_capture = time.time() + self._maximum_background_seconds
+
+    def provide_current_background(self):
+        """Make sure the calibration process works on the current background."""
+        if time.time() > self._next_background_capture:
+            self.record_background()
 
     @staticmethod
     def wait_for_key_press():
@@ -137,28 +79,29 @@ class Calibration:
 
     def record_point(self):
         """Display a point and record it with the camera."""
+        self.provide_current_background()
         image = self.get_resolution_zeros()
         x = int(random.random() * self._resolution[0])
         y = int(random.random() * self._resolution[1])
-        cv2.circle(image, (x, y), circle_radius, 255, -1)
+        cv2.circle(image, (x, y), self._circle_radius, 255, -1)
         cv2.imshow(self._window_name, image)
+        cv2.waitKey(1)
         capture = capture_image_gray()
-        diff = capture // 2 + 128 - background
+        diff = capture // 2 + 128 - self._background
         ret, white = cv2.threshold(diff, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        cv2.imshow("Threshold", white)
+        #cv2.imshow("Threshold", white)
         #images.append(white)
-        # could the that connected components is faster
+        # TODO: evaluate: could the that connected components is faster
         count = 40
         while True:
             last_white = white
-            white = cv2.erode(white, EROSION_SQUARE, iterations=count)
+            white = cv2.erode(white, EROSION_ELEMENT, iterations=count)
             white_pixel_count = cv2.countNonZero(white)
-            print("white_pixel_count", white_pixel_count)
             if white_pixel_count == 0:
                 white = last_white
                 if count >= 4:
                     count //= 4
-                if count >= 2:
+                elif count >= 2:
                     count //= 2
                 else:
                     break # we went too far
@@ -166,7 +109,7 @@ class Calibration:
         xs, ys = cv2.split(white_positions)
         px = xs.mean()
         py = ys.mean()
-        self._source_points.append((x, y)
+        self._source_points.append((x, y))
         self._destination_points.append((px, py))
         self._H = None
 
@@ -176,15 +119,15 @@ class Calibration:
             return
         src = np.array(self._source_points)
         dst = np.array(self._destination_points)
-        assert len(src) == len(dst) >= 4, "Use record_point to get more points. You need at least 4 and have {}.".format(len(dst))
+        assert len(src) == len(dst) >= 4, "Use record_point() to get more points. You need at least 4 and have {}.".format(len(dst))
         H, x = cv2.findHomography(src, dst)
         self._H = H
 
 
 
 if __name__ == "__main__":
-    calibration = Calibration(resolution=max)
-    print("Please move the window to fill the screen.")
+    calibration = Calibration()
+    print("Please move the window to fill the screen and press any key.")
     calibration.wait_for_key_press()
     calibration.record_points(8)
     # calibration.camera_to_projector() # input image
--- pulse_programming.py
+++ pulse_programming.py
@@ -9,60 +9,7 @@ from utils import *
 from color_classification import get_blue_recognition
 from brightness_classification import get_foreground_from_BGR
 
-image_path = (sys.argv[1] if len(sys.argv) > 1 else "explanation.png")
 
-codebook_raw = [[0,0,0], [255, 255, 255], [128,128,128]]
-NUMBER_OF_COLORS = len(codebook_raw)
-OUTPUT_ANIMATION = False
-
-image = camera_image = cv2.imread(image_path)
-DIMENSIONS = (len(image), len(image[0]))
-
-#image = cv2.GaussianBlur(image, (5, 5), 0)
-gate_image = get_blue_recognition(image)
-foreground_image = get_foreground_from_BGR(image)
-road_image = cv2.bitwise_and(cv2.bitwise_not(foreground_image), cv2.bitwise_not(gate_image))
-
-DELATION_ITERATIONS = 3
-EROSION_ITERATIONS = 2
-expansion_element = np.array([[1,1,1], [1,1,1], [1,1,1]])
-gate_image = cv2.erode(gate_image, expansion_element, iterations=EROSION_ITERATIONS)
-gate_image = cv2.dilate(gate_image, expansion_element, iterations=DELATION_ITERATIONS)
-road_image = cv2.dilate(road_image, expansion_element, iterations=DELATION_ITERATIONS)
-road_image = cv2.erode(road_image, expansion_element, iterations=EROSION_ITERATIONS)
-
-road_image = cv2.bitwise_and(road_image, cv2.bitwise_not(gate_image))
-
-
-cv2.namedWindow("road_image", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("road_image", road_image)
-cv2.namedWindow("gate_image", cv2.WINDOW_AUTOSIZE)
-cv2.imshow("gate_image", gate_image)
-
-# pulse generation
-zeros = np.zeros(DIMENSIONS, np.uint8)
-
-road_pulse_element = np.array([[0,1,0],[1,1,1],[0,1,0]], np.uint8)
-right_pulse_element = np.array([[0,0,0],[1,0,0],[0,0,0]], np.uint8)
-down_pulse_element = np.array([[0,1,0],[0,0,0],[0,0,0]], np.uint8)
-nor_pulse_element = np.array([[1,0,0],[0,0,0],[0,0,0]], np.uint8)
-pulse_input_element = np.array([[0,0,1],[0,0,1],[1,1,1]], np.uint8)
-expand_element = np.array([[1,1,1],[1,1,1],[1,1,1]], np.uint8)
-gate_expand_element = np.array([[1,1,1],[1,1,0],[1,0,0]], np.uint8)
-
-# generate image with only generator_element1
-pulse_input_image = cv2.subtract(
-    gate_image,
-    cv2.dilate(gate_image, pulse_input_element))
-gate_expanded = cv2.dilate(gate_image, gate_expand_element)
-
-#cv2.namedWindow("pulse_input_image", cv2.WINDOW_AUTOSIZE)
-#cv2.imshow("pulse_input_image", pulse_input_image)
-
-def output_animation(frame_index, base_path, image):
-    filename, extension = os.path.splitext(base_path)
-    filename += "-animation-frame-{:05}{}".format(frame_index, extension)
-    cv2.imwrite(filename, image)
 
 def pulse(state, element, area, input=()):
     """Make the next pulse using an element on an area, adding the input
@@ -76,33 +23,141 @@ def pulse(state, element, area, input=()):
     pulse2 = cv2.bitwise_and(pulse2, area)
     return pulse2, pulse1
 
-cv2.namedWindow("PULSE", cv2.WINDOW_AUTOSIZE)
-#cv2.namedWindow("pulse_input", cv2.WINDOW_AUTOSIZE)
-down_pulse = right_pulse = road_pulse = (zeros, zeros)
-nor_pulse = zeros
-frame_index = 0
-while cv2.waitKey(1) != 27: # press escape
-    t = time.time()
-    pulse_input = cv2.dilate(cv2.bitwise_and(
-        pulse_input_image, cv2.bitwise_not(
-            cv2.dilate(nor_pulse,
-                expand_element))), expand_element)
-    road_pulse = pulse(road_pulse, road_pulse_element, road_image, (pulse_input, down_pulse[0], right_pulse[0]))
-    right_pulse = pulse(right_pulse, right_pulse_element, gate_expanded, (road_pulse[0],))
-    down_pulse = pulse(down_pulse, down_pulse_element, gate_expanded, (road_pulse[0],))
-    nor_input = cv2.bitwise_and(right_pulse[0], down_pulse[0])
-    nor_pulse = cv2.bitwise_or(nor_input, cv2.dilate(nor_pulse, nor_pulse_element))
-    nor_pulse = cv2.bitwise_and(nor_pulse, gate_image)
-    #nor_pulse = pulse(nor_pulse, nor_pulse_element, gate_expanded, (nor_input,))
-    image = road_pulse[0]
-    for a_pulse, strength in ((right_pulse[0], 3), (down_pulse[0], 3), (nor_pulse, 1)):
-        image = cv2.bitwise_or(image, a_pulse//strength)
 
-#    cv2.imshow("pulse_input", pulse_input)
-    show_image = cv2.bitwise_or(camera_image, cv2.cvtColor(image, cv2.COLOR_GRAY2BGR))
-    cv2.imshow("PULSE", show_image)
-    print("duration", time.time() - t, "frame_index", frame_index)
-    frame_index+= 1
-    if OUTPUT_ANIMATION:
-        output_animation(frame_index, image_path, show_image)
-exit(0)
+class PulseField:
+    """This field computes the pulse.
+
+    Attributes:
+    - DELATION_ITERATIONS - expasion at the beginning of the program to reduce noise
+    - EROSION_ITERATIONS - erosion at the beginning of the program to reduce noise
+    - road_pulse_element - pulse walking along the roads
+    - right_pulse_element - pulse walking through the gate to the right
+    - down_pulse_element - pulse walking down through the gate
+    - and_pulse_element - pulse walking through the gate when down and right pulse join
+    - nand_element - expansion to find NAND positions
+    - expand_element - expansion of the cells to the neighbors
+    - gate_expand_element - expansion of the gate to overlap roads with right and down pulse
+    """
+
+    DELATION_ITERATIONS = 3
+    EROSION_ITERATIONS = 2
+
+    road_pulse_element = np.array([[0,1,0],[1,1,1],[0,1,0]], np.uint8)
+    right_pulse_element = np.array([[0,0,0],[1,0,0],[0,0,0]], np.uint8)
+    down_pulse_element = np.array([[0,1,0],[0,0,0],[0,0,0]], np.uint8)
+    and_pulse_element = np.array([[1,0,0],[0,0,0],[0,0,0]], np.uint8)
+    nand_element = np.array([[0,0,1],[0,0,1],[1,1,1]], np.uint8)
+    expand_element = np.array([[1,1,1],[1,1,1],[1,1,1]], np.uint8)
+    gate_expand_element = np.array([[1,1,1],[1,1,0],[1,0,0]], np.uint8)
+
+    def __init__(self, program_image):
+        """Create a new pulse working on an image."""
+        self.set_program_image(program_image)
+        self.reset_pulse()
+
+    def set_program_image(self, program_image, debug=True):
+        """Set the program image.
+
+        The background should be white, the gates blue and the roads black.
+
+        Parameters:
+        - program_image - BGR image.
+        - debug - whether to open windows with debug information.
+        """
+        self._program_image = program_image
+        self._parse_program_image(debug)
+        self._initialize_pulse_generation()
+
+    def _parse_program_image(self, debug):
+        """Parse the program image to set the road and gates."""
+        program_image = self._program_image
+        gate_image = get_blue_recognition(program_image)
+        foreground_image = get_foreground_from_BGR(program_image)
+        road_image = cv2.bitwise_and(
+            cv2.bitwise_not(foreground_image),
+            cv2.bitwise_not(gate_image))
+
+        gate_image = cv2.erode(gate_image, self.expand_element, iterations=self.EROSION_ITERATIONS)
+        gate_image = cv2.dilate(gate_image, self.expand_element, iterations=self.DELATION_ITERATIONS)
+        road_image = cv2.dilate(road_image, self.expand_element, iterations=self.DELATION_ITERATIONS)
+        road_image = cv2.erode(road_image, self.expand_element, iterations=self.EROSION_ITERATIONS)
+
+        road_image = cv2.bitwise_and(road_image, cv2.bitwise_not(gate_image))
+
+        if debug:
+            cv2.namedWindow("road_image", cv2.WINDOW_AUTOSIZE)
+            cv2.imshow("road_image", road_image)
+            cv2.namedWindow("gate_image", cv2.WINDOW_AUTOSIZE)
+            cv2.imshow("gate_image", gate_image)
+        self._gate_image = gate_image
+        self._road_image = road_image
+
+    def _initialize_pulse_generation(self):
+        """Based on roads and gates, initialize the pulse generation."""
+        gate_image = self._gate_image
+        self._pulse_input_image = cv2.subtract(
+            gate_image,
+            cv2.dilate(gate_image, self.nand_element))
+        self._gate_expanded = cv2.dilate(gate_image, self.gate_expand_element)
+
+    def reset_pulse(self):
+        """Set the pulse state to the beginnig of the program."""
+        zeros = np.zeros(self._road_image.shape()[:2], np.uint8)
+        self._down_pulse = self._right_pulse = self._road_pulse = (zeros, zeros)
+        self._and_pulse = zeros
+
+    def pulse(self):
+        """Do one step of pulsing."""
+        pulse_input = cv2.dilate(cv2.bitwise_and(
+            self._pulse_input_image, cv2.bitwise_not(
+                cv2.dilate(self._and_pulse,
+                    expand_element))), expand_element)
+        self._road_pulse = pulse(
+            self._road_pulse, self.road_pulse_element, self._road_image,
+            (pulse_input, self._down_pulse[0], self._right_pulse[0]))
+        self._right_pulse = pulse(
+            self._right_pulse, self.right_pulse_element, self._gate_expanded,
+            (self._road_pulse[0],))
+        self._down_pulse = pulse(
+            self._down_pulse, self.down_pulse_element, self._gate_expanded,
+            (self._road_pulse[0],))
+        # and pulse
+        and_input = cv2.bitwise_and(right_pulse[0], down_pulse[0])
+        and_pulse = cv2.bitwise_or(
+            self._and_input, cv2.dilate(self._and_pulse, self.and_pulse_element))
+        self._and_pulse = cv2.bitwise_and(and_pulse, self._gate_image)
+
+    def get_pulse_gray(self):
+        """Return the pulse image of the pulse."""
+        image = self._road_pulse[0]
+        pulses = (
+            (self._right_pulse[0], 3),
+            (self._down_pulse[0], 3),
+            (self._and_pulse, 1)
+        )
+        for a_pulse, strength in pulses:
+            image = cv2.bitwise_or(image, a_pulse//strength)
+        return image
+
+    def get_pulse_colored(self):
+        """Return the pulse image colored."""
+        gray = self.get_image_gray()
+        return cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+
+    def get_pulse_on_program(self):
+        """Return the pulse image rendered on the program image"""
+        colored = self.get_image_colored()
+        return cv2.bitwise_or(self._program_image, colored)
+
+
+
+if __name__ == "__main__":
+    image_path = (sys.argv[1] if len(sys.argv) > 1 else "explanation.png")
+    image = camera_image = cv2.imread(image_path)
+    cv2.namedWindow("PULSE", cv2.WINDOW_AUTOSIZE)
+    pulse_field = PulseField(image)
+    while cv2.waitKey(1) != 27:
+        t = time.time()
+        pulse_field.pulse()
+        print("duration:", time.time() - t)
+        cv2.imshow("PULSE", pulse_field.get_pulse_on_program())
